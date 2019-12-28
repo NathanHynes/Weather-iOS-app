009b18cdbcab6aa86a78f0178ce4aa9d022d43a7
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-12-28 15:55:29 +0000

    unpacked JSON response and assigned values

--- Weather2/ViewController.swift
+++ Weather2/ViewController.swift
@@ -26,13 +26,53 @@ class ViewController: UIViewController {
     
     let gradientLayer = CAGradientLayer()
     
+    let apiKey = "e2fc0cf4b9cd08d88affab3621247ded"
+    var lat = 51.594164
+    var lon = -0.130986
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setBlueGradientBackground()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+            }
+        }
     }
     
     func setBlueGradientBackground(){
