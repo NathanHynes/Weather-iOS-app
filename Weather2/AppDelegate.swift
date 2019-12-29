d1f5ce77385607e6e75f097aa3867510370674c7
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-12-29 15:45:47 +0000

    added launch screen

--- Weather2/AppDelegate.swift
+++ Weather2/AppDelegate.swift
@@ -12,10 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.extendSplashScreenPresentation()
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,6 +33,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func extendSplashScreenPresentation(){
+        // Get a refernce to LaunchScreen.storyboard
+        let launchStoryBoard = UIStoryboard.init(name: "LaunchScreen", bundle: nil)
+        // Get the splash screen controller
+        let splashController = launchStoryBoard.instantiateViewController(withIdentifier: "splashController")
+        // Assign it to rootViewController
+        self.window?.rootViewController = splashController
+        self.window?.makeKeyAndVisible()
+        // Setup a timer to remove it after n seconds
+        Timer.scheduledTimer(timeInterval: 15, target: self, selector: #selector(dismissSplashController), userInfo: nil, repeats: false)
+    }
+    
+    @objc private func dismissSplashController() {
+        // Get a refernce to Main.storyboard
+        let mainStoryBoard = UIStoryboard.init(name: "Main", bundle: nil)
+        // Get initial viewController
+        let initController = mainStoryBoard.instantiateViewController(withIdentifier: "initController")
+        // Assign it to rootViewController
+        self.window?.rootViewController = initController
+        self.window?.makeKeyAndVisible()
     }
 
 
